5502b0efbd87c0b55e936db8adfd3d4668eb119d
chirag_php_dev <chirag.koshti@whizsolutions.co.uk>
2023-12-30 14:50:23 +0530

    added migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name', 255)->nullable();
+            $table->string('last_name', 255)->nullable();
+            $table->date('dob')->nullable();
+            $table->string('contact_no', 13)->nullable();
+            $table->string('gender', 6)->nullable();
+            $table->string('pic', 255)->nullable();
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
